9a19578111d45a48349e3fc41e192b333bf7a34b
Jacob Drury <jacob.drury@waitrapp.com>
2023-06-09 00:09:12 -0500

    setup spotify api

--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module github.com/jacobdrury/spotify-duplicates
 go 1.18
 
 require (
+	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/zmb3/spotify/v2 v2.3.1 // indirect
-	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
-	golang.org/x/oauth2 v0.0.0-20210810183815-faf39c7919d5 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
 
 	"github.com/joho/godotenv"
-	spotifyauth "github.com/zmb3/spotify/v2/auth"
-
-	"golang.org/x/oauth2/clientcredentials"
-
-	"github.com/zmb3/spotify/v2"
 )
 
 func main() {
@@ -19,44 +12,47 @@ func main() {
         log.Fatalf("Error loading environment variables file")
     }
 
-	ctx := context.Background()
-	config := &clientcredentials.Config{
-		ClientID:     os.Getenv("SPOTIFY_ID"),
-		ClientSecret: os.Getenv("SPOTIFY_SECRET"),
-		TokenURL:     spotifyauth.TokenURL,
-	}
-	token, err := config.Token(ctx)
-	if err != nil {
-		log.Fatalf("couldn't get token: %v", err)
-	}
+	var spotify = SpotifyClient{}
+	spotify.Init()
+	
+	// ctx := context.Background()
+	// config := &clientcredentials.Config{
+	// 	ClientID:     os.Getenv("SPOTIFY_ID"),
+	// 	ClientSecret: os.Getenv("SPOTIFY_SECRET"),
+	// 	TokenURL:     spotifyauth.TokenURL,
+	// }
+	// token, err := config.Token(ctx)
+	// if err != nil {
+	// 	log.Fatalf("couldn't get token: %v", err)
+	// }
 
-	httpClient := spotifyauth.New().Client(ctx, token)
-	client := spotify.New(httpClient)
+	// httpClient := spotifyauth.New().Client(ctx, token)
+	// client := spotify.New(httpClient)
 
-	// Public playlist owned by noah.stride:
-	// "Long playlist for testing pagination"
-	playlistID := "3LsZV4IGAzA0yi59XEEPr3?si=a7c167d4381e4162"
-	if id := os.Getenv("SPOTIFY_PLAYLIST"); id != "" {
-		playlistID = id
-	}
+	// // Public playlist owned by noah.stride:
+	// // "Long playlist for testing pagination"
+	// playlistID := "3LsZV4IGAzA0yi59XEEPr3?si=a7c167d4381e4162"
+	// if id := os.Getenv("SPOTIFY_PLAYLIST"); id != "" {
+	// 	playlistID = id
+	// }
 
-	tracks, err := client.GetPlaylistItems(
-		ctx,
-		spotify.ID(playlistID),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// tracks, err := client.GetPlaylistItems(
+	// 	ctx,
+	// 	spotify.ID(playlistID),
+	// )
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	log.Printf("Playlist has %d total tracks", tracks.Total)
-	for page := 1; ; page++ {
-		log.Printf("  Page %d has %d tracks", page, len(tracks.Items))
-		err = client.NextPage(ctx, tracks)
-		if err == spotify.ErrNoMorePages {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	// log.Printf("Playlist has %d total tracks", tracks.Total)
+	// for page := 1; ; page++ {
+	// 	log.Printf("  Page %d has %d tracks", page, len(tracks.Items))
+	// 	err = client.NextPage(ctx, tracks)
+	// 	if err == spotify.ErrNoMorePages {
+	// 		break
+	// 	}
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// }
 }
