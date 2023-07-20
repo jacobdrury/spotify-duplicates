dbc84c6b9c1bcb95245fa2ecba44df6cea9393b9
Jacob Drury <jacob.drury@waitrapp.com>
2023-07-20 00:47:53 -0500

    Build out raw duplicate removal process

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/jacobdrury/spotify-duplicates
 go 1.18
 
 require (
-	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute v1.20.1 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/zmb3/spotify/v2 v2.3.1 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,58 +1,164 @@
 package main
 
 import (
-	"log"
-
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/jacobdrury/spotify-duplicates/utils"
 	"github.com/joho/godotenv"
+	"github.com/zmb3/spotify/v2"
+	"github.com/zmb3/spotify/v2/auth"
+	"log"
+	"net/http"
+)
+
+// redirectURI is the OAuth redirect URI for the application.
+// You must register an application at Spotify's developer portal
+// and enter this value.
+const redirectURI = "http://localhost:8080/callback"
+
+var (
+	auth  *spotifyauth.Authenticator
+	ch    = make(chan *spotify.Client)
+	state = "abc123"
 )
 
 func main() {
-	err := godotenv.Load(".env")
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+
+	auth = spotifyauth.New(
+		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithScopes(
+			spotifyauth.ScopeUserReadPrivate,
+			spotifyauth.ScopePlaylistModifyPrivate,
+			spotifyauth.ScopePlaylistModifyPublic,
+			spotifyauth.ScopePlaylistReadPrivate,
+			spotifyauth.ScopePlaylistReadCollaborative,
+		))
+
+	// first start an HTTP server
+	http.HandleFunc("/callback", completeAuth)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Got request for:", r.URL.String())
+	})
+
+	go func() {
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	url := auth.AuthURL(state)
+	fmt.Println("Please log in to Spotify by visiting the following page in your browser:", url)
+	utils.OpenBrowser(url)
+
+	// wait for auth to complete
+	client := <-ch
+
+	// use the client to make calls that require authorization
+	user, err := client.CurrentUser(context.Background())
 	if err != nil {
-        log.Fatalf("Error loading environment variables file")
-    }
+		log.Fatal(err)
+	}
+	fmt.Println("You are logged in as:", user.ID)
 
-	var spotify = SpotifyClient{}
-	spotify.Init()
-	
-	// ctx := context.Background()
-	// config := &clientcredentials.Config{
-	// 	ClientID:     os.Getenv("SPOTIFY_ID"),
-	// 	ClientSecret: os.Getenv("SPOTIFY_SECRET"),
-	// 	TokenURL:     spotifyauth.TokenURL,
-	// }
-	// token, err := config.Token(ctx)
-	// if err != nil {
-	// 	log.Fatalf("couldn't get token: %v", err)
-	// }
+	usersPlayLists := make([]spotify.SimplePlaylist, 0)
 
-	// httpClient := spotifyauth.New().Client(ctx, token)
-	// client := spotify.New(httpClient)
+	pageLimit := 50
+	pageOffSet := 0
+	hasNext := true
+	for hasNext {
+		playlistPage, err := client.CurrentUsersPlaylists(context.Background(), spotify.Limit(pageLimit), spotify.Offset(pageOffSet))
 
-	// // Public playlist owned by noah.stride:
-	// // "Long playlist for testing pagination"
-	// playlistID := "3LsZV4IGAzA0yi59XEEPr3?si=a7c167d4381e4162"
-	// if id := os.Getenv("SPOTIFY_PLAYLIST"); id != "" {
-	// 	playlistID = id
-	// }
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// tracks, err := client.GetPlaylistItems(
-	// 	ctx,
-	// 	spotify.ID(playlistID),
-	// )
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+		//fmt.Printf("Found %d of playlists\n", len(playlistPage.Playlists))
 
-	// log.Printf("Playlist has %d total tracks", tracks.Total)
-	// for page := 1; ; page++ {
-	// 	log.Printf("  Page %d has %d tracks", page, len(tracks.Items))
-	// 	err = client.NextPage(ctx, tracks)
-	// 	if err == spotify.ErrNoMorePages {
-	// 		break
-	// 	}
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// }
+		for _, playlist := range playlistPage.Playlists {
+			if playlist.Owner.ID == user.ID && playlist.ID == "52RPoUq4YfBUXbZO7WKix0" {
+				usersPlayLists = append(usersPlayLists, playlist)
+			}
+		}
+
+		hasNext = playlistPage.Next != ""
+		pageOffSet += (pageOffSet + 1) * pageLimit
+	}
+
+	fmt.Printf("Found a total of %d playlists owned by %s\n", len(usersPlayLists), user.ID)
+
+	for _, playlist := range usersPlayLists {
+		duplicateTracks := make(map[spotify.ID]spotify.TrackToRemove, 0)
+		hashMap := make(map[string]spotify.ID)
+		itemsLimit := 50
+		itemsPageOffset := 0
+		itemsHasNext := true
+
+		for itemsHasNext {
+			fullPlaylist, err := client.GetPlaylistItems(context.Background(), playlist.ID, spotify.Limit(itemsLimit), spotify.Offset(itemsPageOffset))
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for i, item := range fullPlaylist.Items {
+				if hashMap[item.Track.Track.SimpleTrack.Name] != "" {
+					if existingTrackToRemove, found := duplicateTracks[item.Track.Track.SimpleTrack.ID]; found {
+						// The track is already in duplicateTracks. Add the position to the Positions slice.
+						existingTrackToRemove.Positions = append(existingTrackToRemove.Positions, i+itemsPageOffset)
+						duplicateTracks[item.Track.Track.SimpleTrack.ID] = existingTrackToRemove
+					} else {
+						// The track is not yet in duplicateTracks. Add it with the current position.
+						duplicateTracks[item.Track.Track.SimpleTrack.ID] = spotify.TrackToRemove{
+							URI:       string(item.Track.Track.SimpleTrack.URI),
+							Positions: []int{i + itemsPageOffset},
+						}
+					}
+				}
+
+				hashMap[item.Track.Track.Name] = item.Track.Track.SimpleTrack.ID
+				//item.AddedAt
+			}
+
+			itemsHasNext = fullPlaylist.Next != ""
+			itemsPageOffset += (itemsPageOffset + 1) * itemsLimit
+		}
+
+		prettyPrintJson(duplicateTracks)
+		fmt.Printf("Removing %d duplicate tracks in %s\b\n", len(duplicateTracks), playlist.Name)
+		//_, err := client.RemoveTracksFromPlaylistOpt(context.Background(), playlist.ID, maps.Values(duplicateTracks), "")
+		//
+		//if err != nil {
+		//	log.Fatal(err)
+		//}
+	}
+}
+
+func completeAuth(w http.ResponseWriter, r *http.Request) {
+	tok, err := auth.Token(r.Context(), state, r)
+	if err != nil {
+		http.Error(w, "Couldn't get token", http.StatusForbidden)
+		log.Fatal(err)
+	}
+	if st := r.FormValue("state"); st != state {
+		http.NotFound(w, r)
+		log.Fatalf("State mismatch: %s != %s\n", st, state)
+	}
+
+	// use the token to get an authenticated client
+	client := spotify.New(auth.Client(r.Context(), tok))
+	fmt.Fprintf(w, "Login Completed!")
+	ch <- client
+}
+
+func prettyPrintJson(j any) {
+	marshaled, err := json.MarshalIndent(j, "", "   ")
+	if err != nil {
+		log.Fatalf("marshaling error: %s", err)
+	}
+	fmt.Println(string(marshaled))
 }
